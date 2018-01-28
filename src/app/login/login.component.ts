ce0b83076cf9b93978c8eb7eb9d3d582ae7083b1
Ritwick Dey <ritwickdey@outlook.com>
2018-01-28 20:37:00 +0530

    routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ILoginData } from './../models/auth.model';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {}
 
-  login(data) {
+  login(data: ILoginData) {
    
   }
 }
