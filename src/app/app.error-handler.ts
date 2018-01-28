da09cec00b9cfe48510c4a880bc782476b51041b
Ritwick Dey <ritwickdey@outlook.com>
2018-01-29 00:49:55 +0530

    toast added

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,10 +1,14 @@
 import { ErrorHandler } from '@angular/core';
+import {MaterializeAction} from "angular2-materialize";
+
+declare var Materialize;
 
 export class AppErrorHandler implements ErrorHandler {
   handleError(err: any): void {
     // Oh My God! error.error.error.... :/
     if (err && err.error && err.error.error && err.error.error.message) {
       console.log(err.error.error.message);
+      Materialize.toast(err.error.error.message, 4000)
     }
 
     console.log('Handled By GLOBAL', err);
