fcab3a85df57c43e50fcd1292646859623372bbb
Ritwick Dey <ritwickdey@outlook.com>
2018-01-28 20:54:34 +0530

    oops! mistake

--- src/app/MagicString.ts
+++ src/app/MagicString.ts
@@ -2,4 +2,4 @@ import { isDevMode } from '@angular/core'
 
 export const TOKEN = "token";
 export const REFRESH_TOKEN = "refreshToken";
-export const RESOURCE_URL = isDevMode ? "http://localhost:4200" : '<PRODUCTION-URL>';
+export const RESOURCE_URL = isDevMode ? "http://localhost:3000" : '<PRODUCTION-URL>';
