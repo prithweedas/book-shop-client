ccd90530376e568d8569611aa31e86fd9c2f8175
Ritwick Dey <ritwickdey@outlook.com>
2018-01-28 13:16:30 +0530

    Materialize CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { MaterializeModule } from "angular2-materialize";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -18,7 +18,8 @@ import { TokenService } from './services/token.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MaterializeModule
   ],
   providers: [
     TokenService,
