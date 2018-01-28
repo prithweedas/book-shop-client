22da62cedfddd23a7f83329bfe6d365c124a1412
Prithwee Das <prithweedas123@gmail.com>
2018-01-28 21:49:07 +0530

    dynamic years instead of hard coded data

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  yearOptions;
+  constructor() {}
 
   ngOnInit() {
+    const year = new Date().getFullYear();
+    this.yearOptions = Array.from(new Array(4), (x, i) => ({
+      value: year + i,
+      name: year + i
+    }));
   }
-
+  signUp(data) {
+    console.log(data);
+  }
 }
