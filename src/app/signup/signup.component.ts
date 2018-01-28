88bdda326d70b362d64d980a9979b9dac2358011
Prithwee Das <prithweedas123@gmail.com>
2018-01-28 21:27:18 +0530

    signup component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  yearOptions;
+  constructor() {}
 
   ngOnInit() {
+    const year = new Date().getFullYear();
+    this.yearOptions = [
+      { value: year, name: year },
+      { value: year + 1, name: year + 1 },
+      { value: year + 2, name: year + 2 },
+      { value: year + 3, name: year + 3 }
+    ];
   }
-
+  signUp(data) {
+    console.log(data);
+  }
 }
