bdbf5e65e362981edd7ff557de1b34fac02341f8
Prithwee Das <prithweedas123@gmail.com>
2018-01-29 21:18:22 +0530

    single quotes

--- src/app/MagicString.ts
+++ src/app/MagicString.ts
@@ -1,5 +1,7 @@
-import { isDevMode } from '@angular/core'
+import { isDevMode } from '@angular/core';
 
-export const TOKEN = "token";
-export const REFRESH_TOKEN = "refreshToken";
-export const RESOURCE_URL = isDevMode ? "http://localhost:3000" : '<PRODUCTION-URL>';
+export const TOKEN = 'token';
+export const REFRESH_TOKEN = 'refreshToken';
+export const RESOURCE_URL = isDevMode
+  ? 'http://localhost:3000'
+  : '<PRODUCTION-URL>';
