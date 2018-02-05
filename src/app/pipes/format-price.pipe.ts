792e001c6bd6e40665bfc70280c6aa7122a9da6f
Prithwee Das <prithweedas123@gmail.com>
2018-02-06 00:52:49 +0530

    added price formatting for million

--- src/app/pipes/format-price.pipe.ts
+++ src/app/pipes/format-price.pipe.ts
@@ -6,7 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FormatPricePipe implements PipeTransform {
   transform(value: string): any {
     let val = parseFloat(value);
-    if (val > 1000) {
+    if (val >= 1000000) {
+      return (val / 1000000).toFixed(1) + 'M';
+    }
+    if (val >= 1000) {
       return (val / 1000).toFixed(1) + 'K';
     }
 
