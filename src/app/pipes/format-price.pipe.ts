71b3824cd5f596ff329793d0a503a8b5af032dc6
Prithwee Das <prithweedas123@gmail.com>
2018-02-06 00:35:49 +0530

    format price pipe

---
+++ src/app/pipes/format-price.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatPrice'
+})
+export class FormatPricePipe implements PipeTransform {
+  transform(value: string): any {
+    let val = parseFloat(value);
+    if (val > 1000) {
+      return (val / 1000).toFixed(1) + 'K';
+    }
+
+    return val;
+  }
+}
